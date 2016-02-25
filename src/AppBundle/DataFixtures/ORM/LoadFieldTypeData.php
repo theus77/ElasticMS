1b390aff44817afc0c68c89c8a1790ce4db06397
theus deka <me@theus.be>
2016-02-25 18:09:45 +0100

    merged ?

--- src/AppBundle/DataFixtures/ORM/LoadFieldTypeData.php
+++ src/AppBundle/DataFixtures/ORM/LoadFieldTypeData.php
@@ -5,23 +5,20 @@ namespace AppBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\FieldType;
-use AppBundle\Entity\ContentType;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 
 class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterface
 {
 	//private $currentTime;
-	private $isRootFieldType;
 	
 	public function load(ObjectManager $manager)
 	{
-		$this->isRootFieldType = false;
 		//$this->currentTime = time(); //date("Y-m-d H:i:s");
 		//Create fields for CT label
 		$labelFields = array(
 				//contentType id, parent id --> should both be set dynamically
 				//type, name, label, deleted, orderKey, many, icon
-				['Container', 'datafield', '', 0, 0, 0, ''],
+				['Container', 'dataField', '', 0, 0, 0, ''],
 				['Ouuid', 'key', 'key', 0, 0, 0, 'fa fa-key'],
 				['Container', 'translations', 'Translations', 0, 0, 0, 'fa fa-language'],
 				['String', 'value_en', 'English', 0, 0, 0, ''],
@@ -29,7 +26,7 @@ class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterfa
 				['String', 'value_nl', 'Nederlands', 0, 0, 0, ''],
 		);
 		$richTextFields = array(
-				['Container', 'datafield', '', 0, 0, 0, ''],
+				['Container', 'dataField', '', 0, 0, 0, ''],
 		);
 		
 		$fields = array(
@@ -43,18 +40,10 @@ class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterfa
 			$containerField = null;
 			foreach ($data as $fieldData)
 			{
+				
 				$fieldType = $this->createFieldType($contentTypeName, $containerField, ...$fieldData);
 				$manager->persist($fieldType);
 				$manager->flush();
-				
-				if ($this->isRootFieldType)
-				{
-					$this->isRootFieldType = false;
-					$contentTypeEntity = $this->getReference($contentTypeName);
-					$contentTypeEntity->setFieldType($fieldType);
-					$manager->persist($contentTypeEntity);
-					$manager->flush();
-				}
 				
 				//If container this field will be the new parent for the next fields
 				//TODO or not?: this does not work for recursive containers, except in a straight line (= the order of your fields is very importan in this strategy)
@@ -69,7 +58,7 @@ class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterfa
 						$this->setReference($containerField, $fieldType);
 					} catch (\OutOfBoundsException $e) {
 						//if error occured the reference does not exist and we can add it
-						$this->addReference($containerField, $fieldType);						
+						$this->addReference($containerField, $fieldType);
 					}
 				}
 			}
@@ -88,6 +77,12 @@ class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterfa
 	private function createFieldType($contentType, $parent, $type, $name, $label, $deleted, $orderKey, $many, $icon)
 	{
 		$fieldType = new FieldType();
+		if ($parent != ''){
+			$fieldType->setParent($this->getReference($parent));
+		}else{
+			$fieldType->setContentType($this->getReference($contentType));
+			$this->getReference($contentType)->setFieldType($fieldType);
+		}
 		//$fieldType->setCreated($this->currentTime);
 		//$fieldType->setModified($this->currentTime);
 		$fieldType->setType('AppBundle\\Form\\'.$type.'Type');
@@ -97,14 +92,6 @@ class LoadFieldTypeData extends AbstractFixture implements OrderedFixtureInterfa
 		$fieldType->setOrderKey($orderKey);
 		$fieldType->setMany($many);
 		//$fieldType->setIcon($icon);
-		if ($parent != ''){
-			$fieldType->setParent($this->getReference($parent));
-		}else{
-			/** @var ContentType $contentTypeEntity */
-			$fieldType->setContentType($this->getReference($contentType));
-			//Setting the content type means we are it's root fieldtype, so we should add that relationship also!
-			$this->isRootFieldType = true;
-		}
 		
 		return $fieldType;
 	}
