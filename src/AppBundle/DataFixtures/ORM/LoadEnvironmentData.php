d2bbc8245a5d79459ad59180cf1a6e953e884f3c
David Meert <david@davidmeert.be>
2016-02-25 08:47:19 +0100

    Add fixtures for environment, content type, field type.
    There is an issue with ORM relations (oneToOne against OneToMany looks
    impossible?

---
+++ src/AppBundle/DataFixtures/ORM/LoadEnvironmentData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use AppBundle\Entity\Environment;
+
+class LoadEnvironmentData extends AbstractFixture implements OrderedFixtureInterface
+{
+	//private $currentTime;
+	
+	public function load(ObjectManager $manager)
+	{
+		//$this->currentTime =  time();
+		
+		$environmentsData = array(
+				//name, color, managed
+				['draft', 'lightblue', 1],
+				['staging', 'blue', 1],
+				['live', 'green', 1],
+		);
+
+		foreach ($environmentsData as $environmentData)
+		{
+			$environment = $this->createEnvironment(...$environmentData);
+			$manager->persist($environment);
+			$manager->flush();
+
+			$name = $environmentData[0];
+			$this->addReference($name, $environment);
+		}
+
+	
+	}
+	
+	public function getOrder()
+	{
+		// the order in which fixtures will be loaded
+		// the lower the number, the sooner that this fixture is loaded
+		return 1;
+	}
+	
+	private function createEnvironment($name, $color, $managed)
+	{
+		$environment = new Environment();
+	//	$environment->setCreated($this->currentTime);
+	//	$environment->setModified($this->currentTime);
+		$environment->setName($name);
+		$environment->setColor($color);
+		$environment->setManaged($managed);
+		
+		return $environment;
+	}
+}
