c74f1b8ef9339b300ae8569ff701554969affc13
theus deka <me@theus.be>
2016-03-15 18:22:32 +0100

    unpublish object

--- src/AppBundle/Controller/ContentManagement/PublishController.php
+++ src/AppBundle/Controller/ContentManagement/PublishController.php
@@ -11,6 +11,7 @@ use AppBundle\Repository\RevisionRepository;
 use Doctrine\ORM\EntityManager;
 use AppBundle\Entity\Revision;
 use AppBundle\Entity\Environment;
+use Elasticsearch\Client;
 
 class PublishController extends AppController
 {
@@ -63,6 +64,64 @@ class PublishController extends AppController
 				'type' => $revision->getContentType()->getName(),
 				'body' => $objectArray
 		]);
+		
+		$em->persist($revision);
+		$em->flush();
+		
+		return $this->redirectToRoute('data.revisions', [
+				'ouuid' => $revision->getOuuid(),
+				'type'=> $revision->getContentType()->getName()
+		]);
+		
+	}
+	/**
+	 * @Route("/revision/unpublish/{revisionId}/{envId}", name="revision.unpublish"))
+	 */
+	public function unpublishAction($revisionId, $envId, Request $request)
+	{
+		/** @var EntityManager $em */
+		$em = $this->getDoctrine()->getManager();
+		
+		/** @var RevisionRepository $revisionRepo */
+		$revisionRepo = $em->getRepository('AppBundle:Revision');
+		
+		/** @var Revision $revision */
+		$revision = $revisionRepo->find($revisionId);
+		
+		if(!$revision) {
+			throw $this->createNotFoundException('Revision not found');
+		}
+		
+
+		/** @var RevisionRepository $revisionRepo */
+		$environmentRepo = $em->getRepository('AppBundle:Environment');
+		
+		/** @var Environment $environment */
+		$environment = $environmentRepo->find($envId);
+		
+		if(!$environment) {
+			throw $this->createNotFoundException('Environment not found');
+		}
+		
+		$revision->removeEnvironment($environment);
+		
+
+		/** @var Client $client */
+		$client = $this->get('app.elasticsearch');
+		
+		try {
+			$status = $client->delete([
+					'id' => $revision->getOuuid(),
+					'index' => $environment->getAlias(),
+					'type' => $revision->getContentType()->getName(),
+			]);
+			$this->addFlash('notice', 'The object has been unpublished from environment '.$environment->getName());
+		}
+		catch(Missing404Exception $e){
+			if(!$revision->getDeleted()) {
+				$this->addFlash('warning', 'The object was already unpublished from environment '.$environment->getName());
+			}
+		}
 		
 		$em->persist($revision);
 		$em->flush();
